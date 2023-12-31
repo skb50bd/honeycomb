f27234703c6fa4f0eb603db4c9e1b0894af705e8
Shakib Haris <skb50bd@gmail.com>
2023-12-31 12:32:39 +0600

    Added envsubst to generate /root/cloudflare.ini

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@
 # https://certbot-dns-cloudflare.readthedocs.io/en/stable/
 sudo apt update && \
 sudo apt install -y certbot python3-certbot-dns-cloudflare && \
-sudo certbot certonly --dns-cloudflare --dns-cloudflare-credentials /root/cloudflare.ini -d ${BASE_DOMAIN},*.${BASE_DOMAIN}
+sudo certbot certonly --dns-cloudflare --dns-cloudflare-credentials /root/cloudflare.ini -d ${BASE_DOMAIN},*.${BASE_DOMAIN} && \
+sudo envsubst < cloudflare.ini > /root/cloudflare.ini
